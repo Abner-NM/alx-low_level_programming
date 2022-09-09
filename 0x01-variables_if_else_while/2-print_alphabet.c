5b5ba81f430832a1a596bfd04a3752ca1a862521
Abner Nyangena <abnernyangena@gmail.com>
2022-09-09 09:34:26 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints alphabet in lowercase
- * Return: Always (0) if success
+ * Return: Always 0 if success
  */
 int main(void)
 {
@@ -12,6 +12,6 @@ int main(void)
 	{
 		putchar(c);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
