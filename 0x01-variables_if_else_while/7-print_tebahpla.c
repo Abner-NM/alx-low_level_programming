3252e9d1301df6687e13e331e0e348b7b493ad79
Abner Nyangena <abnernyangena@gmail.com>
2022-09-09 12:02:58 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet
+ * in reverse
+ * followed by a new line
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	char c;
+	for (c='z'; c >='a'; z--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
