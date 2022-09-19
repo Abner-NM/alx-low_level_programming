813d18bb87492c4f9723e5e0e156b5d2fcd33383
Abner Nyangena <abnernyangena@gmail.com>
2022-09-19 12:28:58 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	k = j - 1;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[k];
+		s[k--] = tmp;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -5,4 +5,5 @@
 2. 2-strlen.c - a function that returns the length of a string
 3. 3-puts.c - a function that prints a string, followed by a new line, to stdout
 4. 4-print_rev.c - a function that prints a string, in reverse, followed by a new line
+5. 5-rev_string.c - a function that reverses a string
 
