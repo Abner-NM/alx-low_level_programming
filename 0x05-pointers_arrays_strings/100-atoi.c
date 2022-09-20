ef41ee4b21a74bc3fc94c0b9c555662bc5d22b81
Abner Nyangena <abnernyangena@gmail.com>
2022-09-19 20:49:09 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,8 +12,8 @@ int _atoi(char *s)
 	int i;
 	int n = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-		n = n * 10 + str[i] - '0';
+	for (i = 0; s[i] != '\0'; i++)
+		n = n * 10 + s[i] - '0';
 
 	retrun (n);
 }
