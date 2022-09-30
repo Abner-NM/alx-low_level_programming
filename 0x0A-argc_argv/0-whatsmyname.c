f2f35a925f24be1fb0365a04b770114035cd0917
Abner Nyangena <abnernyangena@gmail.com>
2022-09-30 07:56:01 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name,
+ * followed by a new line
+ * @argc: number of arguements
+ * @argv: string of arguements
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)),char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
