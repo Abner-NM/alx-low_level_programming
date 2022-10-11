9132be3fbdc5d4d8b86bb5b8750ca2a008c7f1c6
Abner Nyangena <abnernyangena@gmail.com>
2022-10-11 11:48:44 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,10 @@ struct dog
 /**
  * dog_t - a new typedef for dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
