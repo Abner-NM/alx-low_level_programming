89cdb88d09188fd9d0ce6d35d027bd4b7239cb08
Abner Nyangena <abnernyangena@gmail.com>
2022-10-11 10:40:06 -0700

    Task 0 Done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - type representing a dog's attribites
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
