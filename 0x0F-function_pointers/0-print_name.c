7b657dfbf05788da34de00ef32f09b1ca082c1ee
Abner Nyangena <abnernyangena@gmail.com>
2022-10-12 21:41:03 -0700

    Task 0 v1 - print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to function to print a name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
