65a2d7403d6817579ed1d9355044ef1572ed2e68
Abner Nyangena <abnernyangena@gmail.com>
2022-10-27 21:22:06 -0700

    Task 10 version 3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,22 +10,29 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *temp, *copy = *head;
 	unsigned int node;
 
-	listint_t *temp;
-
-	if (!*head || !head)
+	if (!copy)
 		return (-1);
-	while (*head)
+
+	if (index == 0)
 	{
-		if (index == node)
-		{
-			temp = (*head)->next;
-			free(*head);
-			(*head) = temp;
-			return (1);
-		}
-		node++;
+		*head = (*head)->next;
+		free(copy);
+		return (1);
 	}
-	return (-1);
+
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next == NULL)
+			return (-1);
+
+		copy = copy->next;
+	}
+
+	temp = (*head)->next;
+	(*head)->next = temp->next;
+	free(temp);
+	return (1);
 }
